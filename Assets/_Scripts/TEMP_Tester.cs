2c50071d358b725427f9c3048bfe6076f03366b8
John Pandis <ioanpand@outlook.com>
2025-12-06 02:28:41 +0200

    Up to date -> Scene + Tester script

--- Assets/_Scripts/TEMP_Tester.cs
+++ Assets/_Scripts/TEMP_Tester.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using CocaCopa.Logger.API;
 using CocaCopa.SaveSystem.API;
 using iMoney.Transactions.API;
 using iMoney.Transactions.Contracts;
@@ -6,7 +7,9 @@ using UnityEngine;
 
 public class TEMP_Tester : MonoBehaviour {
     [SerializeField] private bool save1;
-    [SerializeField] private bool load;
+    [SerializeField] private bool remove;
+    [SerializeField] private bool daily;
+    [SerializeField] private bool weekly;
 
     // Update is called once per frame
     void Update() {
@@ -15,9 +18,40 @@ public class TEMP_Tester : MonoBehaviour {
             Transaction tr = Transaction.Create(123123, new TransactionAmount(1200, 10), "Spend", "Games");
             TransactionsManager.AddEntry(tr);
         }
-        if (load) {
-            load = false;
-            TransactionsManager.RemoveEntry("1");
+        if (remove) {
+            remove = false;
+            Debug.Log("Debug removing entry doesn't work right now");
+            // TransactionsManager.RemoveEntry("1");
         }
+        if (daily) {
+            daily = false;
+            var entries = TransactionsManager.GetDailyTransactions();
+            foreach (var entry in entries) {
+                Debug.Log($"Type: {entry.Type} | Amount: {entry.Amount.Value / (float)entry.Amount.Multiplier} | Category: {entry.Category}");
+            }
+        }
+        if (weekly) {
+            weekly = false;
+            var week = TransactionsManager.GetCurrentWeekTransactions();
+            for (int i = 0; i < week.Count; i++) {
+                Log.Warning($"Day: {IndexToDay(i)}", LogColor.Yellow);
+                foreach (var entry in week[IndexToDay(i)]) {
+                    Log.Info($"Type: {entry.Type} | Amount: {entry.Amount.Value / (float)entry.Amount.Multiplier} | Category: {entry.Category}", LogColor.White);
+                }
+            }
+        }
+    }
+
+    private string IndexToDay(int index) {
+        return index switch {
+            0 => "Monday",
+            1 => "Tuesday",
+            2 => "Wednesday",
+            3 => "Thursday",
+            4 => "Friday",
+            5 => "Saturday",
+            6 => "Sunday",
+            _ => "Unknown",
+        };
     }
 }
